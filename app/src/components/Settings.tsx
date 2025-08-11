4fd581a1351c2d92dc8fea56934892abd5ebc89b
Azzpect <atanughosh.brp@gmail.com>
2025-08-11 14:32:57 +0530

    completed folder structure

--- app/src/components/Settings.tsx
+++ app/src/components/Settings.tsx
@@ -1,25 +1,26 @@
 import { useState } from "react"
-import { X, ChevronRight, ChevronDown, FolderClosed, File, FileText } from "lucide-react"
+import { X, ChevronRight, ChevronDown, FolderClosed, FileText } from "lucide-react"
 
 
 
 export default function Settings() {
 
     const [ showFolderPicker, setShowFolderPicker ] = useState<boolean>(false)
+    const [ folderPath, setFolderPath ] = useState<string>("")
 
     return (
         <div className="flex flex-col justify-self-center w-4/5 p-10">
             <label htmlFor="path" className="text-lg text-white self-start">Your movies directory:</label>
             <div className="flex gap-2">
-                <input type="text" id="path" className="border-white border-2 outline-none py-1 px-3 w-2/4 text-white text-lg" readOnly />
+                <input type="text" id="path" className="border-white border-2 outline-none py-1 px-3 w-2/4 text-white text-lg" value={folderPath} readOnly />
                 <button className="bg-purple-950 text-white p-3 rounded-xl font-bold text-md cursor-pointer" onClick={() => setShowFolderPicker(true)}>Choose</button>
             </div>
-            {showFolderPicker && <FolderPicker setShowFolderPicker={setShowFolderPicker} />}
+            {showFolderPicker && <FolderPicker setShowFolderPicker={setShowFolderPicker} setFolderPath={setFolderPath} />}
         </div>
     )
 }
 
-function FolderPicker({ setShowFolderPicker } : { setShowFolderPicker : React.Dispatch<React.SetStateAction<boolean>>}) {
+function FolderPicker({ setShowFolderPicker, setFolderPath } : { setShowFolderPicker : React.Dispatch<React.SetStateAction<boolean>>, setFolderPath: React.Dispatch<React.SetStateAction<string>>}) {
 
     const folderData: FolderData = {
         name: "/",
@@ -60,10 +61,10 @@ function FolderPicker({ setShowFolderPicker } : { setShowFolderPicker : React.Di
     }
 
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-h-[40vh] min-w-[20vw] bg-white">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[40vh] min-w-[20vw] bg-white">
             <X className="absolute top-0 right-0 cursor-pointer" onClick={() => setShowFolderPicker(false)} />
             <h2 className="text-lg font-bold p-2">Choose a folder:</h2>
-            <FolderStructure folderData={folderData} />
+            <FolderStructure folderData={folderData} setFolderPath={setFolderPath} />
         </div>
     )
 }
@@ -79,34 +80,47 @@ const Icons : {[key : string]: any} = {
     "txt": <FileText />
 }
 
-function FolderStructure({ folderData } : { folderData: FolderData}) {
+function FolderStructure({ folderData, setFolderPath } : { folderData: FolderData, setFolderPath: React.Dispatch<React.SetStateAction<string>> }) {
+
+    const [ selectedFolder, setSelectedFolder ] = useState<string>("")
+
     return (
-        <div className="p-5">
-            <Folder data={folderData} indent={1} />
+        <div className="flex flex-col h-full">
+            <div className="p-5 h-4/6 box-border">
+                <Folder data={folderData} indent={1} selectedFolder={selectedFolder} setSelectedFolder={setSelectedFolder} />
+            </div>
+            <button className="self-end mr-10 bg-blue-400 rounded-2xl text-white py-2 px-3 cursor-pointer" disabled={selectedFolder === ""} onClick={() => setFolderPath(selectedFolder)}>Choose</button>
         </div>
     )
 }
 
-function Folder({ data, indent } : { data: FolderData, indent: number }) {
+function Folder({ data, indent, path, selectedFolder, setSelectedFolder } : { data: FolderData, indent: number, path?: string, selectedFolder: string, setSelectedFolder: React.Dispatch<React.SetStateAction<string>> }) {
 
     const [ open, setOpen ] = useState<boolean>(false)
-    
+    path = path ? path : data.name
+
     return (
         <>
             <div 
-            className="flex items-center gap-2 cursor-pointer"
+            className={`flex items-center gap-2 cursor-pointer ${selectedFolder === path ? "bg-blue-300" : ""} hover:bg-blue-300`}
             style={{paddingLeft: `${indent*8}px`}}
             onClick={() => {
                 if (!open) setOpen(true)
+                if (data.type === "dir") setSelectedFolder(path)
             }} 
             >
-                {!open ? <ChevronRight className="cursor-pointer" /> : <ChevronDown className="cursor-pointer" onClick={() => setOpen(false)} />}
+                {!open ? <ChevronRight className="cursor-pointer" /> : <ChevronDown className="cursor-pointer" 
+                onClick={() => {
+                    setOpen(false)
+                    setSelectedFolder("")
+                    }
+                } />}
                 {Icons[data.type]}
                 <p className="text-lg">{data.name}</p>
             </div>
             { open && data.children &&
                 data.children.map((child, i) => {
-                    return <Folder key={i} data={child} indent={indent+1} />
+                    return <Folder key={i} data={child} indent={indent+1} path={`${path !== "/" ? `${path}/` : "/"}${child.name}`} selectedFolder={selectedFolder} setSelectedFolder={setSelectedFolder} />
                 })
             }
         </>
