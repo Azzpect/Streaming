9240748472cf45a5dcacc97c08fa0294ac4e3a11
Azzpect <atanughosh.brp@gmail.com>
2025-08-15 19:52:12 +0530

    created new navbar and removed sidebar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
 import Settings from "./components/Settings";
 import { ToastContainer, Bounce } from "react-toastify";
@@ -38,9 +37,8 @@ function MainContent() {
   const [page, setPage] = useState<"home" | "settings">("home");
 
   return (
-    <div className="w-screen h-screen grid grid-cols-[300px_1fr] grid-rows-[80px_1fr] bg-purple-900">
-      <Navbar />
-      <Sidebar setPage={setPage} />
+    <div className="w-screen h-screen bg-gray-900">
+      <Navbar setPage={setPage} />
       {page === "home" && <Home />}
       {page === "settings" && <Settings />}
     </div>
--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,8 +1,18 @@
+import { Search } from "lucide-react"
 
 
-
-export default function Navbar() {
+export default function Navbar({ setPage } : { setPage: React.Dispatch<React.SetStateAction<"home" | "settings">>}) {
     return (
-        <h1 className="col-span-full bg-purple-950 flex items-center text-white font-bold pl-5 text-2xl">Streaming APP</h1>
+        <div className="w-full h-[12vh] flex items-start justify-around pt-5">
+            <h1 className="flex items-center text-white font-bold pl-5 text-2xl">Streaming <span className="text-cyan-400">APP</span></h1>
+            <div className="w-1/4 h-3/7 self-end relative">
+                <input type="text" className="w-full h-full p-3 pl-5 outline-none text-lg text-white border-2 border-white rounded-4xl" />
+                <Search color="white" className="absolute top-1/2 -translate-y-1/2 right-4" />
+            </div>
+            <nav className="h-full flex gap-10">
+                <li className="font-bold text-gray-300 text-lg list-none cursor-pointer transition-colors duration-300 ease-linear hover:text-cyan-400" onClick={() => setPage("home")}>Home</li>
+                <li className="font-bold text-gray-300 text-lg list-none cursor-pointer transition-colors duration-300 ease-linear hover:text-cyan-400" onClick={() => setPage("settings")}>Settings</li>
+            </nav>
+        </div>
     )
 }
