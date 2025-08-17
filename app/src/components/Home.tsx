d1d6075375ad35c55d1bfae931694acfb5d6fda0
Azzpect <atanughosh.brp@gmail.com>
2025-08-17 17:52:19 +0530

    fixed scan loading

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -10,22 +10,20 @@ export default function Home() {
     useContext(MediaDataContext)!;
   const [loading, setLoading] = useState<boolean>(false);
 
-  function startMediaProcessing() {
+  async function startMediaProcessing() {
     setLoading(true);
     resetAllMedia();
     try {
-      fetch(`${import.meta.env.VITE_API_URL}/process-media`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/process-media`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.status === "error") throw new Error(data.message);
-          updateMediaList(data.data);
-          toast.success("Media processed successfully");
-        });
+      });
+      const data = await res.json();
+      if (data.status === "error") throw new Error(data.message);
+      updateMediaList(data.data);
+      toast.success("Media processed successfully");
     } catch (err) {
       toast.error((err as Error).message);
     } finally {
@@ -54,64 +52,109 @@ export default function Home() {
             Scanning
           </button>
         )}
-        {allMedia.length === 0 ? <p className="font-bold text-white text-xl text-center">No media found</p> : <Slider mediaList={allMedia} />}
+        {allMedia.length === 0 ? (
+          <p className="font-bold text-white text-xl text-center">
+            No media found
+          </p>
+        ) : (
+          <Slider mediaList={allMedia} />
+        )}
       </div>
     </>
   );
 }
 
 function Slider({ mediaList }: { mediaList: Media[] }) {
-  const window = useRef<HTMLElement>(null)
-  const slider = useRef<HTMLDivElement>(null)
-  const [offset, setOffset] = useState<number>(0)
-  const [activeCard, setActiveCard] = useState<number>(Math.floor(mediaList.length / 2))
+  const window = useRef<HTMLElement>(null);
+  const slider = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState<number>(0);
+  const [activeCard, setActiveCard] = useState<number>(
+    Math.floor(mediaList.length / 2)
+  );
 
   useEffect(() => {
-    if (!slider.current) return
-    slider.current.style.translate = `${offset}px 0`
-  }, [offset])
+    if (!slider.current) return;
+    slider.current.style.translate = `${offset}px 0`;
+  }, [offset]);
 
   useEffect(() => {
-    setActiveCard(Math.floor(mediaList.length / 2))
-  }, [mediaList])
+    setActiveCard(Math.floor(mediaList.length / 2));
+  }, [mediaList]);
 
   return (
     <section ref={window} className="w-full h-4/5 overflow-hidden relative">
-      <div ref={slider} className="flex items-center h-full gap-20 absolute top-0 left-1/3 -translate-x-1/2 transition-transform duration-300 ease-linear">
-        {
-          mediaList.map((m, i) => <MediaCard key={i} name={m.name} thumbnail={m.thumbnail} i={i}  activeCard={activeCard} setActiveCard={setActiveCard} setOffset={setOffset} windowObj={window} />)
-        }
+      <div
+        ref={slider}
+        className="flex items-center h-full gap-20 absolute top-0 left-1/3 -translate-x-1/2 transition-transform duration-300 ease-linear"
+      >
+        {mediaList.map((m, i) => (
+          <MediaCard
+            key={i}
+            name={m.name}
+            thumbnail={m.thumbnail}
+            i={i}
+            activeCard={activeCard}
+            setActiveCard={setActiveCard}
+            setOffset={setOffset}
+            windowObj={window}
+          />
+        ))}
       </div>
     </section>
-  )
+  );
 }
 
-function MediaCard({ i, name, thumbnail, setOffset, activeCard, setActiveCard, windowObj }: { i : number, name: string, thumbnail: string, activeCard: number, setActiveCard: React.Dispatch<React.SetStateAction<number>>, setOffset: React.Dispatch<React.SetStateAction<number>>, windowObj: React.RefObject<HTMLElement | null>}) {
-
-  const child = useRef<HTMLDivElement>(null)
-  const navigate = useNavigate()
-  const parts = window.location.href.split(":")
-  const MEDIA_URL = "http:" + parts.filter((_, i) => i !== parts.length - 1 && i !== 0).join("")+":8100"
+function MediaCard({
+  i,
+  name,
+  thumbnail,
+  setOffset,
+  activeCard,
+  setActiveCard,
+  windowObj,
+}: {
+  i: number;
+  name: string;
+  thumbnail: string;
+  activeCard: number;
+  setActiveCard: React.Dispatch<React.SetStateAction<number>>;
+  setOffset: React.Dispatch<React.SetStateAction<number>>;
+  windowObj: React.RefObject<HTMLElement | null>;
+}) {
+  const child = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+  const parts = window.location.href.split(":");
+  const MEDIA_URL =
+    "http:" +
+    parts.filter((_, i) => i !== parts.length - 1 && i !== 0).join("") +
+    ":8100";
 
   function slide() {
-    if (!windowObj.current) return
-    if (!child.current) return
-    if (activeCard === i) navigate("/player?id="+i)
-    const windowRect = windowObj.current.getBoundingClientRect()
-    const childRect = child.current.getBoundingClientRect()
-    const distance = (windowRect.left + windowRect.width / 2) - (childRect.left + childRect.width / 2)
-    setOffset(prev => prev + distance)
-    setActiveCard(i)
+    if (!windowObj.current) return;
+    if (!child.current) return;
+    if (activeCard === i) navigate("/player?id=" + i);
+    const windowRect = windowObj.current.getBoundingClientRect();
+    const childRect = child.current.getBoundingClientRect();
+    const distance =
+      windowRect.left +
+      windowRect.width / 2 -
+      (childRect.left + childRect.width / 2);
+    setOffset((prev) => prev + distance);
+    setActiveCard(i);
   }
 
   useEffect(() => {
-    if (!child.current) return
-    if (activeCard === i) child.current.style.zIndex = "5"
-    child.current.style.scale = `${150 - (Math.abs(activeCard - i) * 20)}%`
-  }, [activeCard])
+    if (!child.current) return;
+    if (activeCard === i) child.current.style.zIndex = "5";
+    child.current.style.scale = `${150 - Math.abs(activeCard - i) * 20}%`;
+  }, [activeCard]);
 
   return (
-    <div ref={child} className={`w-45 h-60 flex flex-col items-center justify-center cursor-pointer transition-transform duration-300 ease-liear`} onClick={slide}>
+    <div
+      ref={child}
+      className={`w-45 h-60 flex flex-col items-center justify-center cursor-pointer transition-transform duration-300 ease-liear`}
+      onClick={slide}
+    >
       <img
         src={new URL(thumbnail, MEDIA_URL).href}
         alt={name}
@@ -119,5 +162,5 @@ function MediaCard({ i, name, thumbnail, setOffset, activeCard, setActiveCard, w
       />
       {/* <h2 className="text-white font-bold text-lg text-center">{name}</h2> */}
     </div>
-  )
+  );
 }
